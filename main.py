bbf80d19f1b669a909fa6054e6067379455074c6
Hung Dao <hung.dao-nguyen@epitech.eu>
2019-11-05 17:39:17 -0800

    test randomly several sets

--- main.py
+++ main.py
@@ -2,19 +2,37 @@ import numpy as np
 import matplotlib.pyplot as pt
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+import random 
 
 data = pd.read_csv("data/train.csv").as_matrix()
+
 clf = DecisionTreeClassifier()
 
-xtrain = data[0:21000, 1:]
-train_label = data[0:21000, 0]
-clf.fit(xtrain, train_label)
+train_inputs = data[0:21000, 1:]
+train_outputs = data[0:21000, 0]
+test_inputs = data[21000:, 1:]
+test_outputs = data[21000:, 0]
 
-xtest = data[21000:, 1:]
-actual_label = data[21000:, 0]
+# train the classifier
+clf.fit(train_inputs, train_outputs)
 
-d = xtest[8]
-d.shape = (28, 28)
-pt.imshow(255-d, cmap='gray')
-print(clf.predict( [xtest[8]] ))
-pt.show()
+# test
+print("\tPredicted Result\t|\tCorrect Result\t")
+nb_tests = 9
+correct_predictions_count = 0.0
+for i in range(0, nb_tests):
+    # take random set from the dataset
+    test_index = random.randint(0, len(test_inputs) - 1)
+    predicted_result = clf.predict([test_inputs[test_index]])
+    correct_result = test_outputs[test_index]
+    
+    if predicted_result == correct_result:
+        correct_predictions_count += 1.0
+
+    print("\t\t%d\t\t|\t\t%d\t" % (predicted_result, correct_result))
+
+    d = test_inputs[test_index]
+    d.shape = (28, 28)
+    pt.imshow(255-d, cmap='gray')
+    pt.show()
+print("Accuracy: %.2f%%" % (correct_predictions_count / nb_tests * 100.0))
